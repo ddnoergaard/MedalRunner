11f18e5f5bd39fddf05eb069e6ab355fc370b717
TheOneTrueBear <45456681+TheOneTrueBear@users.noreply.github.com>
2026-05-05 11:54:34 +0200

    Changes to Dungeon Properties Added GetAllDungeons in dungeonrepository

--- MedalRunner/Models/Dungeon.cs
+++ MedalRunner/Models/Dungeon.cs
@@ -6,6 +6,11 @@
         public string Name { get; set; }
         public string Zone { get; set; }
         public string Description { get; set; }
+        public bool Platinum { get; set; }
+        public bool Gold { get; set; }
+        public bool Silver { get; set; }
+        public bool Bronze { get; set; }
+        public int Mop_amount { get; set; }
         public List<Boss> Bosses { get; set; }
 
         public Dungeon()
--- MedalRunner/Repositories/DungeonRepository.cs
+++ MedalRunner/Repositories/DungeonRepository.cs
@@ -1,4 +1,8 @@
-﻿using MedalRunner.Repositories.Interfaces;
+﻿using MedalRunner.Models;
+using MedalRunner.Repositories.Interfaces;
+using Microsoft.Data.SqlClient;
+using System.ComponentModel;
+using System.Reflection.PortableExecutable;
 
 namespace MedalRunner.Repositories
 {
@@ -9,6 +13,47 @@ namespace MedalRunner.Repositories
         public DungeonRepository(IConfiguration configuration)
         {
             _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<List<Dungeon>> GetAllDungeons()
+        {
+            //Create a list to store all the dungeons that we retrieve from the database
+            List<Dungeon> data = new List<Dungeon>();
+            //Create Instance of dungeon for later use
+            Dungeon dungeon = new Dungeon();
+
+            string sqlQuery = "Select * FROM Dungeons";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                SqlCommand cmd = new SqlCommand(sqlQuery, connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                try
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        dungeon.Id = reader.GetInt32(reader.GetOrdinal("id"));
+                        dungeon.Name = reader.GetString(reader.GetOrdinal("name"));
+                        dungeon.Zone = reader.GetString(reader.GetOrdinal("zone"));
+                        dungeon.Description = reader.GetString(reader.GetOrdinal("description"));
+                        dungeon.Platinum = reader.GetBoolean(reader.GetOrdinal("platinum"));
+                        dungeon.Gold = reader.GetBoolean(reader.GetOrdinal("gold"));
+                        dungeon.Silver = reader.GetBoolean(reader.GetOrdinal("silver"));
+                        dungeon.Bronze = reader.GetBoolean(reader.GetOrdinal("bronze"));
+                        dungeon.Mop_amount = reader.GetInt32(reader.GetOrdinal("mop_amount"));
+
+                        data.Add(dungeon);
+                    }
+                    return data;
+                }
+                catch(SqlException ex)
+                {
+                    throw;
+                }
+
+            }
         }
     }
 }
