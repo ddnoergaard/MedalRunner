87150fd31cadedce4647432f6cf2fc96ce8f7449
TheOneTrueBear <45456681+TheOneTrueBear@users.noreply.github.com>
2026-05-07 12:03:31 +0200

    Added character delete page

---
+++ MedalRunner/Pages/App/Character/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MedalRunner.Pages.App.Character
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
