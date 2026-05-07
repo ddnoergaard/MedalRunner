bd888e86da13c03c7a8a76bb82f9ba2b3692921d
Luxsuss <axelkknudsen@gmail.com>
2026-05-07 12:04:32 +0200

    Implemented delete character backend

--- MedalRunner/Pages/App/Character/Delete.cshtml.cs
+++ MedalRunner/Pages/App/Character/Delete.cshtml.cs
@@ -1,3 +1,4 @@
+using MedalRunner.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,29 @@ namespace MedalRunner.Pages.App.Character
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private ICharacterService _characterService;
+
+        public DeleteModel(ICharacterService characterService)
         {
+            _characterService = characterService;
+        }
+
+        [BindProperty]
+        public Models.Character Character { get; set; }
+
+        public async Task<IActionResult> OnGet(int id)
+        {
+            Character = await _characterService.GetById(id);
+            if (Character == null)
+                return RedirectToPage("");
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _characterService.Delete(Character.Id);
+            return RedirectToPage("./Index");
         }
     }
 }
