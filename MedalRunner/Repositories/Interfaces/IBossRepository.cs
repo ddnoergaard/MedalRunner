850549993a56cf2ef217894eee8585dcdde42951
Daniel Nørgaard <ddnoergaard@gmail.com>
2026-05-07 11:53:19 +0200

    Update IBossRepository.cs

--- MedalRunner/Repositories/Interfaces/IBossRepository.cs
+++ MedalRunner/Repositories/Interfaces/IBossRepository.cs
@@ -2,5 +2,11 @@
 {
     public interface IBossRepository
     {
+        Task AddAsync(Boss boss, int dungeonId);
+        Task<IEnumerable<Boss>> GetAllBossesAsync();
+        Task DeleteAsync(int id);
+        Task UpdateImageUrl(int bossId, string imageUrl);
+        Task<Boss> GetBossByIdAsync(int id);
+        Task<Boss> GetBossByNameAsync(string name);
     }
 }
