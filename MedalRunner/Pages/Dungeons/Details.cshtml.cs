cec4e494d9b38fbf86cc458250a74d0405eb8fe3
Daniel Nørgaard <ddnoergaard@gmail.com>
2026-05-07 11:50:19 +0200

    backend to dungeons/details

--- MedalRunner/Pages/Dungeons/Details.cshtml.cs
+++ MedalRunner/Pages/Dungeons/Details.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MedalRunner.Models;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using MedalRunner.Services.Interfaces;
 
 namespace MedalRunner.Pages.Public_pages.Dungeons
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly IDungeonService _dungeonService;
+        public Dungeon Dungeon { get; set; }
+
+        public DetailsModel(IDungeonService dungeonService)
         {
+            _dungeonService = dungeonService;
+        }
+
+        public async Task<IActionResult> OnGet(int id)
+        {
+            Dungeon.Bosses = (await _dungeonService.GetBossesAsync(id)).ToList();
+
+            return Page();
         }
     }
 }
