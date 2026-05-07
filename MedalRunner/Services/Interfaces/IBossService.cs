e9d63a951eb94f98274634246079eb6b3c0fcd47
Daniel Nørgaard <ddnoergaard@gmail.com>
2026-05-07 11:54:22 +0200

    Update IBossService.cs

--- MedalRunner/Services/Interfaces/IBossService.cs
+++ MedalRunner/Services/Interfaces/IBossService.cs
@@ -2,5 +2,11 @@
 {
     public interface IBossService
     {
+        Task AddAsync(Boss boss, int dungeonId);
+        Task<IEnumerable<Boss>> GetBossesAsync();
+        Task DeleteAsync(int id);
+        Task UpdateImageUrl(int bossId, string imageUrl);
+        Task<Boss> GetBossById(int id);
+        Task<Boss> GetBossByNameAsync(string name);
     }
 }
