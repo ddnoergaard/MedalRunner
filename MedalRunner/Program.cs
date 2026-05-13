ddfc1ca4b4f6fa419f59ecf0a734b8370f5cf205
Daniel Nørgaard <ddnoergaard@gmail.com>
2026-05-13 13:45:02 +0200

    Fixed cookieservice and methods for login and logout

--- MedalRunner/Program.cs
+++ MedalRunner/Program.cs
@@ -2,13 +2,37 @@ using MedalRunner.Repositories;
 using MedalRunner.Repositories.Interfaces;
 using MedalRunner.Services;
 using MedalRunner.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/App", "PlayerOnly");
+    options.Conventions.AuthorizeFolder("/Admin", "AdminOnly");
+});
 
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("PlayerOnly", policy => policy.RequireRole("Player"));
+    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+});
 
+//Cookie auth -> Hvor brugere bliver sendt til hvis de ikke har adgang.
+/*
+    Hvis en bruger ikke er logget ind, og prøver at tilgå en auth page, så bliver de redirectet til /Login
+    Hvis en bruger er logget ind, og prøver at tilgå Admin siderne, så bliver de redirected to /Error
+ */
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Login";
+        options.AccessDeniedPath = "/Error";
+    });
+
+//Gør så vi kan access client http context.
+builder.Services.AddHttpContextAccessor();
 
 //DI - REPO
 builder.Services.AddScoped<IBossRepository, BossRepository>();
@@ -33,6 +57,7 @@ builder.Services.AddScoped<IGlyphService, GlyphService>();
 builder.Services.AddScoped<IItemService, ItemService>();
 builder.Services.AddScoped<IScoreboardService, ScoreboardService>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<CookieService>();
 
 var app = builder.Build();
 
@@ -48,6 +73,7 @@ app.UseHttpsRedirection();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
